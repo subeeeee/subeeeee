588bfd4a92e02c6fde052740ad0adea36932c7e7
wz <982409733@qq.com>
2019-03-06 18:08:09 +0800

    创建一个excel表格 写入数据

--- src/demo/excel/excel.vue
+++ src/demo/excel/excel.vue
@@ -34,77 +34,80 @@
         wopts: { bookType: 'xlsx', bookSST: true, type: 'binary' }
       }
     },
-//     methods: {
-//       saveAs(obj, fileName) {//当然可以自定义简单的下载文件实现方式
-//         console.log(URL.createObjectURL)
-//         var tmpa = document.createElement("a");
-//         tmpa.download = fileName || "下载";
-//         tmpa.href = URL.createObjectURL(obj); //绑定a标签
-//         tmpa.click(); //模拟点击实现下载
-//         setTimeout(function () { //延时释放
-//           URL.revokeObjectURL(obj); //用URL.revokeObjectURL()来释放这个object URL
-//         }, 100);
-//       },
-//
-//       downloadExl(data, type) {
-//         var wb = { SheetNames: ['Sheet1'], Sheets: {}, Props: {} };
-// //wb.Sheets['Sheet1'] = XLSX.utils.json_to_sheet(data);//通过json_to_sheet转成单页(Sheet)数据
-//         data = XLSX.utils.json_to_sheet(data);
-// // data["B1"] = { t: "s", v: "asdad" };
-// // data["!merges"] = [{//合并第一行数据[B1,C1,D1,E1]
-// // s: {//s为开始
-// // c: 1,//开始列
-// // r: 0//开始取值范围
-// // },
-// // e: {//e结束
-// // c: 4,//结束列
-// // r: 0//结束范围
-// // }
-// // }];
-//         wb.Sheets['Sheet1'] = data;
-//         this.saveAs(new Blob([this.s2ab(XLSX.write(wb, this.wopts))], { type: "application/octet-stream"}), "这里是下载的文件名" + '.' + (this.wopts.bookType == "biff2" ? "xls" : this. wopts.bookType));
-//       },
-//       s2ab(s) {
-//         if (typeof ArrayBuffer !== 'undefined') {
-//           var buf = new ArrayBuffer(s.length);
-//           var view = new Uint8Array(buf);
-//           for (var i = 0; i != s.length; ++i) view[i] = s.charCodeAt(i) & 0xFF;
-//           return buf;
-//         } else {
-//           var buf = new Array(s.length);
-//           for (var i = 0; i != s.length; ++i) buf[i] = s.charCodeAt(i) & 0xFF;
-//           return buf;
-//         }
-//       },
-//
-//         //这里是我自己从后台获取的对象集合（自己用时获取自己的数据）
-//       flash(url){
-//         if(!url){
-//           url='waterflow/all';
-//         }
-//         this.$http.post(url,{
-//           pageNumber:this.pager.pageNumber,
-//           pageSize:this.pager.pageSize,
-//           pageCount:this.pager.pageCount,
-//           recordCount:this.pager.recordCount
-//         }).then(function(response){
-//           if(response.body.status==1){
-//             this.result= response.body.items;
-//             this.pager=response.body.page;
-//           }else{
-//             if(response.body.status==10101){
-//               this.$Message.info("请登陆!");
-//               this.$router.push ('/');
-//             }
-//             this.$Message.info(response.body.message);
-//           }
-//         }, function(){
-//
-//         });
-//       },
-//
-//     },
     methods: {
+      asdf(){
+
+      },
+      saveAs(obj, fileName) {//当然可以自定义简单的下载文件实现方式
+        console.log(URL.createObjectURL)
+        var tmpa = document.createElement("a");
+        tmpa.download = fileName || "下载";
+        tmpa.href = URL.createObjectURL(obj); //绑定a标签
+        tmpa.click(); //模拟点击实现下载
+        setTimeout(function () { //延时释放
+          URL.revokeObjectURL(obj); //用URL.revokeObjectURL()来释放这个object URL
+        }, 100);
+      },
+
+      downloadExl(data, type) {
+        var wb = { SheetNames: ['Sheet1'], Sheets: {}, Props: {} };
+//wb.Sheets['Sheet1'] = XLSX.utils.json_to_sheet(data);//通过json_to_sheet转成单页(Sheet)数据
+        data = XLSX.utils.json_to_sheet(data);
+// data["B1"] = { t: "s", v: "asdad" };
+// data["!merges"] = [{//合并第一行数据[B1,C1,D1,E1]
+// s: {//s为开始
+// c: 1,//开始列
+// r: 0//开始取值范围
+// },
+// e: {//e结束
+// c: 4,//结束列
+// r: 0//结束范围
+// }
+// }];
+        wb.Sheets['Sheet1'] = data;
+        this.saveAs(new Blob([this.s2ab(XLSX.write(wb, this.wopts))], { type: "application/octet-stream"}), "这里是下载的文件名" + '.' + (this.wopts.bookType == "biff2" ? "xls" : this. wopts.bookType));
+      },
+      s2ab(s) {
+        if (typeof ArrayBuffer !== 'undefined') {
+          var buf = new ArrayBuffer(s.length);
+          var view = new Uint8Array(buf);
+          for (var i = 0; i != s.length; ++i) view[i] = s.charCodeAt(i) & 0xFF;
+          return buf;
+        } else {
+          var buf = new Array(s.length);
+          for (var i = 0; i != s.length; ++i) buf[i] = s.charCodeAt(i) & 0xFF;
+          return buf;
+        }
+      },
+
+        //这里是我自己从后台获取的对象集合（自己用时获取自己的数据）
+      flash(url){
+        if(!url){
+          url='waterflow/all';
+        }
+        this.$http.post(url,{
+          pageNumber:this.pager.pageNumber,
+          pageSize:this.pager.pageSize,
+          pageCount:this.pager.pageCount,
+          recordCount:this.pager.recordCount
+        }).then(function(response){
+          if(response.body.status==1){
+            this.result= response.body.items;
+            this.pager=response.body.page;
+          }else{
+            if(response.body.status==10101){
+              this.$Message.info("请登陆!");
+              this.$router.push ('/');
+            }
+            this.$Message.info(response.body.message);
+          }
+        }, function(){
+
+        });
+      },
+
+    },
+    /*methods: {
       asdf(e){
         console.log(e.target.files)
         this.readExcel(e);
@@ -120,7 +123,7 @@
         }
 
         const fileReader = new FileReader();
-        console.log(fileReader)
+        console.log(fileReader,11)
         fileReader.onload = (ev) => {
           try {
             const data = ev.target.result;
@@ -148,12 +151,12 @@
         };
         fileReader.readAsBinaryString(files[0]);
       }
-    },
+    },*/
 
     mounted:function(){
       // this.flash();
       // console.log(this.XLSX);
-      // this.downloadExl([[1,2,3],[1,2,3],[1,2,3],[1,2,3],[1,2,3]])
+      this.downloadExl([[1,2,3],[1,2,3],[1,2,3],[1,2,3],[1,2,3]])
 
 
       // 创建一个txt文件并下载
