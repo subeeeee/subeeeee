ec761f23bf3abf8a2c9744245c3dca06f30b4d2f
subeeeee <41566229+subeeeee@users.noreply.github.com>
2019-07-25 11:17:25 +0800

    Update filters.js
    filter change   try by brower

--- vue-project/src/filters/filters.js
+++ vue-project/src/filters/filters.js
@@ -1,7 +1,12 @@
 import Vue from 'vue'
 
-
-
-Vue.filter('sex',(val)=>{
+Vue.filter('sex',(sex)=>{
+  if(sex === '1'){
+    return 'man'
+     }else if(sex === '2'){
+     return 'woman'
+     }
+})
+Vue.filter('formate',(val)=>{
   return 'asdfasdfasdfasdf'
 })
